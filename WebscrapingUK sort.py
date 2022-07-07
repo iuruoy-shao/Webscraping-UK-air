beb0b26ce2c8cf76a9afe1ec9bc6e01b5d9c6fae
yshao0712 <70494538+yshao0712@users.noreply.github.com>
2022-07-06 22:50:19 -0700

    Update WebscrapingUK sort.py

--- WebscrapingUK sort.py
+++ WebscrapingUK sort.py
@@ -1,15 +1,12 @@
-from bs4 import BeautifulSoup
 import requests
+import re
+from bs4 import BeautifulSoup
 
-# I put here my own blog url ,you can change it.
 url="https://uk-air.defra.gov.uk/latest/currentlevels?view=site"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'}
 
 result = requests.get(url,headers=headers)
 soup=BeautifulSoup(result.text,'html')
-# print(result.content.decode())
-
-import re
 
 links = []
 for link in soup.findAll('a', attrs={'href': re.compile("../networks/site-info")}):
@@ -30,25 +27,33 @@ for full_link in full_links:
   site_id = full_link.replace('http://uk-air.defra.gov.uk/networks/site-info?site_id=', '')
   result2 = requests.get(full_link,headers=headers)
   soup2 = BeautifulSoup(result2.text,'html')
+
   list_uk_air_id = soup2.find_all(string=re.compile("UKA"))
   uk_air_id = (list_uk_air_id[2]).replace(' ','')
+
   list_eu_site_id = soup_test.find_all(string=re.compile("GB"))
   eu_site_id=(list_eu_site_id[0]).replace(' ','')
+
   list_name = soup2.find_all(string=re.compile("Site Information for"))
   fullname = list_name[0]
+
   fullname2,b=fullname.split("(")
   name = fullname2.replace('    Site Information for ', '')
+
   list_region = soup2.findAll('p')
   regionfull = "".join(str(list_region))
   regionfull2,c = regionfull.split('<p><strong>Easting/Northing:</strong> ')
   d,region2 = regionfull2.split('<p><strong>Government Region:</strong> ')
   region = region2.replace('</p>, ','')
+    
   e,locationfull = regionfull.split('<p><strong>Latitude/Longitude:</strong> ')
   locationfull2 = locationfull.split('</p>, <p>')
+    
   coordinates = locationfull2[0]
   latitude,longitude = coordinates.split(', ')
   worldlocation = geolocator.reverse(coordinates, exactly_one=True)
   address = worldlocation.raw['address']
   city = address.get('city', '')
+
   if city == '':
     print(uk_air_id + ',' + eu_site_id + ',' + site_id + ',' + name + ',' + city + ',' + region + ',' + latitude + ',' + longitude)
